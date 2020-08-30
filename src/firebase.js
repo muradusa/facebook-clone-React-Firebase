b3c9d4ad68e52fc17a1f4dc544304f4b391aae2c
Murad Cholukov <muradusa@gmail.com>
2020-08-29 19:09:18 -0700

    finished with header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDzdTgCxfXo0ctldOgG73ViQDEgo1MjmY",
+    authDomain: "facebook-clone-dfa38.firebaseapp.com",
+    databaseURL: "https://facebook-clone-dfa38.firebaseio.com",
+    projectId: "facebook-clone-dfa38",
+    storageBucket: "facebook-clone-dfa38.appspot.com",
+    messagingSenderId: "701281566037",
+    appId: "1:701281566037:web:f61ff364dad9e6bfb003dc",
+    measurementId: "G-PNRN0VGD09"
+  };
